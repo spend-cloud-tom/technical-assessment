2277b96b6c9cb157218a084cf9e05171d79bc4ca
IdinKhayami <idin.khanoom.khayami@gmail.com>
2024-06-05 13:54:05 +0200

    Added new header component

---
+++ components/main-header/main-header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: false,
+    default: '/',
+  },
+})
+</script>
+
+<template>
+  <header
+    class="flex flex-row align-center justify-between border-b border-b-4 border-slate-500 p-8 bg-slate-950 items-center"
+  >
+    <h1 class="text-xl mb-4 md:mb-0">
+      {{ title }}
+    </h1>
+    <nuxt-link :to="link" class="ml-0 md:ml-auto">
+      Go back
+    </nuxt-link>
+  </header>
+</template>
